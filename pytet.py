6219c6f1049c736a5d09ff213c5d1b0a38fed355
Seunghyun Heo <lyk1517@naver.com>
2020-11-20 13:48:40 +0900

    pytet.py added

---
+++ pytet.py
@@ -0,0 +1,181 @@
+from matrix import *
+import random
+
+def draw_matrix(m):
+    array = m.get_array()
+    for y in range(m.get_dy()):
+        for x in range(m.get_dx()):
+            if array[y][x] == 0:
+                print("□", end='')
+            elif array[y][x] == 1:
+                print("■", end='')
+            else:
+                print("XX", end='')
+        print()
+
+
+###
+### initialize variables
+###     
+def rd_array(r):
+    arr = []
+    if r==1:
+        arr.append( [ 0, 0, 1, 0 ])
+        arr.append( [ 0, 0, 1, 0 ])
+        arr.append( [ 0, 0, 1, 0 ])
+        arr.append( [ 0, 0, 1, 0 ])
+    elif r==2:
+        arr.append( [ 1, 1 ])
+        arr.append( [ 1, 1 ])
+    elif r==3:
+        arr.append( [ 0, 1, 0 ])
+        arr.append( [ 0, 1, 0 ])
+        arr.append( [ 1, 1, 0 ])
+    elif r==4:
+        arr.append( [ 0, 0, 0 ])
+        arr.append( [ 1, 1, 1 ])
+        arr.append( [ 0, 1, 0 ])
+    elif r==5:
+        arr.append( [ 0, 1, 0 ])
+        arr.append( [ 0, 1, 1 ])
+        arr.append( [ 0, 0, 1 ])
+    elif r==6:
+        arr.append( [ 0, 1, 0 ])
+        arr.append( [ 0, 1, 0 ])
+        arr.append( [ 0, 1, 1 ])
+    elif r==7:
+        arr.append( [ 0, 0, 0 ])
+        arr.append( [ 1, 1, 0 ])
+        arr.append( [ 0, 1, 1 ])
+# I,O,J,T,S,L,Z
+    return arr
+
+def transpose(matrix):
+    n = len(matrix)
+    cmp = [[0] * n for _ in range(n)]
+    for r in range(n):
+        for i in range(n):
+            cmp[i][n-1-r] = matrix[r][i]
+   
+    return cmp
+
+r = random.randrange(1,8)
+arrayBlk = rd_array(r)
+
+### integer variables: must always be integer!
+iScreenDy = 15
+iScreenDx = 10
+iScreenDw = 4
+top = 0
+left = iScreenDw + iScreenDx//2 - 2
+
+newBlockNeeded = False
+
+arrayScreen = [
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] ]
+
+###
+### prepare the initial screen output
+###  
+iScreen = Matrix(arrayScreen)
+oScreen = Matrix(iScreen)
+currBlk = Matrix(arrayBlk)
+tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+tempBlk = tempBlk + currBlk
+oScreen.paste(tempBlk, top, left)
+draw_matrix(oScreen); print()
+
+###
+### execute the loop
+###
+
+while True:
+    key = input('Enter a key from [ q (quit), a (left), d (right), s (down), w (rotate), \' \' (drop) ] : ')
+    if key == 'q':
+        print('Game terminated...')
+        break
+    elif key == 'a': # move left
+        left -= 1
+    elif key == 'd': # move right
+        left += 1
+    elif key == 's': # move down
+        top += 1
+    elif key == 'w': # rotate the block clockwise
+        arrayBlk = transpose(arrayBlk)
+        currBlk = Matrix(arrayBlk)
+    elif key == ' ': # drop the block
+        while (1):
+            top += 1
+            tempBlk = iScreen.clip(top,left,top+currBlk.get_dy(),left+currBlk.get_dx())
+            tempBlk = tempBlk + currBlk
+            if (tempBlk.anyGreaterThan(1) == True):
+                break
+    else:
+        print('Wrong key!!!')
+        continue
+
+    tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+    tempBlk = tempBlk + currBlk
+    if tempBlk.anyGreaterThan(1):
+        if key == 'a': # undo: move right
+            left += 1
+        elif key == 'd': # undo: move left
+            left -= 1
+        elif key == 's': # undo: move up
+            top -= 1
+            newBlockNeeded = True
+        elif key == 'w': # undo: rotate the block counter-clockwise
+            for i in range(3):
+                arrayBlk = transpose(arrayBlk)
+            currBlk = Matrix(arrayBlk)
+
+        elif key == ' ': # undo: move up
+             top -= 1
+             newBlockNeeded = True
+
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+
+    oScreen = Matrix(iScreen)
+    oScreen.paste(tempBlk, top, left)
+    draw_matrix(oScreen); print()
+
+    if newBlockNeeded:
+        iScreen = Matrix(oScreen)
+        top = 0
+        left = iScreenDw + iScreenDx//2 - 2
+        newBlockNeeded = False
+        r = random.randrange(1,8)
+        arrayBlk = rd_array(r)
+        currBlk = Matrix(arrayBlk)
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+        if tempBlk.anyGreaterThan(1):
+            print('Game Over!!!')
+            break
+        
+        oScreen = Matrix(iScreen)
+        oScreen.paste(tempBlk, top, left)
+        draw_matrix(oScreen); print()
+        
+###
+### end of the loop
+###
